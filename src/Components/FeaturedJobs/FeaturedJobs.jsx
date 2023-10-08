de9f1574d92f2f37b0a0bf497330e794450ecbed
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-10-08 22:42:54 +0200

    Add Icons and handle Show All jobs

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [dataLenght, setDataLength] = useState(4);
 
     useEffect(() => {
         fetch('jobs.json')
@@ -19,10 +20,14 @@ const FeaturedJobs = () => {
                 </div>
                 <div className="grid grid-cols-2 gap-6">
                     {
-                        jobs.map(job => <Job key={job.id} job={job}></Job>)
+                        jobs.slice(0, dataLenght).map(job => <Job key={job.id} job={job}></Job>)
                     }
 
                 </div>
+            </div>
+            <div className="dataLenght === jobs.length && 'hidden'">
+                <button
+                    onClick={() => setDataLength(jobs.length)} className="btn btn-primary ">See All Jobs</button>
             </div>
         </div>
     );
--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { MdOutlineLocationOn } from "react-icons/md";
+import { AiOutlineDollarCircle } from "react-icons/ai";
+
 
 
 const Job = ({ job }) => {
@@ -12,8 +15,16 @@ const Job = ({ job }) => {
                     <button className="px-5 py-2 font-extrabold border rounded text-[#9873FF]  border-[#7E9] mr-4">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded text-[#9873FF] border-[#7E9] mr-4">{job_type}</button>
                 </div>
+                <div className="flex">
+                    <div className="mr-4">
+                        <h2 className="flex"><MdOutlineLocationOn className="text-2xl mr-2"></MdOutlineLocationOn>{location}</h2>
+                    </div>
+                    <div>
+                        <h2 className="flex"><AiOutlineDollarCircle className="text-2xl mr-2" ></AiOutlineDollarCircle>{salary}</h2>
+                    </div>
+                </div>
                 <div className="card-actions ">
-                    <button className="btn btn-primary">View Details</button>
+                    <button className="btn btn-primary text-[#FFF] bg-gradient-to-r from-indigo-500 rounded">View Details</button>
                 </div>
             </div>
         </div>
